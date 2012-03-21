6748527d54f4de14c64517e6e6018a529e3fdbeb
Mike Kazantsev <mk.fraggod@gmail.com>
2012-03-22 02:43:11 +0600

    Basic fixes, first working version

--- ulog-pcap-recv.py
+++ ulog-pcap-recv.py
@@ -8,22 +8,41 @@ def main():
 		description='Receive pcap stream from UDP and push it to a fifo socket.')
 	parser.add_argument('src', help='ZMQ socket address to bind to.')
 	parser.add_argument('dst', help='Path to fifo to write stream to.')
+	parser.add_argument('--debug', action='store_true', help='Verbose operation mode.')
 	optz = parser.parse_args()
+
+	from contextlib import closing
+	from time import sleep
+	import errno, logging
+
+	logging.basicConfig(
+		level=logging.DEBUG if optz.debug else logging.WARNING,
+		logfmt='%(asctime)s :: %(levelname)s :: %(name)s: %(message)s',
+		datefmt='%Y-%m-%d %H:%M:%S' )
+	log = logging.getLogger('pcap_recv')
 
 	optz.reopen = True # only fifo dst for now
 
 	import zmq
 
 	context = zmq.Context()
-	src = context.socket(zmq.PULL)
-	src.bind(optz.src)
 
-	while True:
-		try:
-			with open(optz.dst, 'wb') as dst:
-				pkt = src.recv()
-				dst.write(pkt)
-		except OSError as err:
-			if err.errno == errno.ENOENT: break
-		if not optz.reopen: break
-		sleep(1)
+	with closing(context.socket(zmq.PULL)) as src:
+		src.bind(optz.src)
+
+		while True:
+			try:
+				with open(optz.dst, 'wb') as dst:
+					log.debug('(Re-)opened destination path')
+					while True:
+						pkt = src.recv()
+						dst.write(pkt)
+			except OSError as err:
+				if err.errno == errno.ENOENT: break
+			if not optz.reopen: break
+			sleep(1)
+
+	log.debug('Finishing')
+	context.term()
+
+if __name__ == '__main__': main()
--- ulog-pcap-send.py
+++ ulog-pcap-send.py
@@ -10,34 +10,50 @@ def main():
 	parser.add_argument('--reopen', action='store_true',
 		help='Keep re-opening the path on eof or any read errors (as long as it exists).'
 			' Used implicitly for FIFO sockets (to allow writer process to be restarted).')
-	parser.add_argument('-b', '--buffer', action='store', type=int, default=100,
+	parser.add_argument('-b', '--buffer', type=int, default=100,
 		help='ZMQ_HWM for the socket - number of packets to'
 			' buffer in RAM before dropping (default: %(default)s).')
+	parser.add_argument('--debug', action='store_true', help='Verbose operation mode.')
 	optz = parser.parse_args()
 
-	import os, sys, stat, errno
+	from contextlib import closing
 	from struct import unpack
 	from time import sleep
+	import os, stat, errno, logging
 
-	optz.reopen = stat.ISFIFO(os.stat(optz.path).st_mode)
+	logging.basicConfig(
+		level=logging.DEBUG if optz.debug else logging.WARNING,
+		logfmt='%(asctime)s :: %(levelname)s :: %(name)s: %(message)s',
+		datefmt='%Y-%m-%d %H:%M:%S' )
+	log = logging.getLogger('pcap_send')
+
+	optz.reopen = stat.S_ISFIFO(os.stat(optz.src).st_mode)
 	pcap_hdr = 8 + 4 + 4 # pcap_timeval 2xint32, caplen uint32, len uint32
 
 	import zmq
 	context = zmq.Context()
-	dst = context.socket(zmq.PUSH)
-	dst.setsockopt(zmq.ZMQ_HWM, optz.buffer)
-	dst.connect(optz.dst)
 
-	while True:
-		try:
-			with open(optz.src, 'rb') as src:
-				pkt_hdr = src.read(pcap_hdr)
-				pkt_len = unpack('=I', buff[8:12])
-				if dst.poll(0): # don't block, just discard the packet
-					dst.send(pkt_hdr + src.read(pkt_len))
-		except OSError as err:
-			if err.errno == errno.ENOENT: break
-		if not optz.reopen: break
-		sleep(1)
+	with closing(context.socket(zmq.PUSH)) as dst:
+		dst.setsockopt(zmq.HWM, optz.buffer)
+		dst.setsockopt(zmq.LINGER, 0) # it's lossy either way
+		dst.connect(optz.dst)
+
+		while True:
+			try:
+				with open(optz.src, 'rb') as src:
+					log.debug('(Re-)opened source path')
+					while True:
+						pkt_hdr = src.read(pcap_hdr)
+						if not pkt_hdr: break
+						pkt_len, = unpack('=I', pkt_hdr[8:12])
+						dst.send(pkt_hdr + src.read(pkt_len), zmq.NOBLOCK)
+			except OSError as err:
+				if err.errno == errno.ENOENT: break
+			if not optz.reopen: break
+			sleep(1)
+			break
+
+	log.debug('Finishing')
+	context.term()
 
 if __name__ == '__main__': main()
