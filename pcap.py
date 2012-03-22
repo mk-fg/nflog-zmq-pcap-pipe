d2520e6d10b5c41b1c2c0b71b29f5be64c40be9c
Mike Kazantsev <mk.fraggod@gmail.com>
2012-03-23 00:32:14 +0600

    pcap: style consistency

--- pcap.py
+++ pcap.py
@@ -8,22 +8,24 @@ import ctypes, xdrlib
 
 class PcapError(Exception): pass
 
-class pcap_timeval(ctypes.Structure):
+class c_pcap_timeval(ctypes.Structure):
 	_fields_ = [
 		('tv_sec', ctypes.c_int32),
 		('tv_usec', ctypes.c_int32) ]
-class pcap_pkthdr(ctypes.Structure):
+
+class c_pcap_pkthdr(ctypes.Structure):
 	_fields_ = [
-		('ts', pcap_timeval),
+		('ts', c_pcap_timeval),
 		('caplen', ctypes.c_uint32),
 		('len', ctypes.c_uint32) ]
 
 
-def ReadCheck(pcap_t, err, func, args):
+def _chk_read(pcap_t, err, func, args):
 	if err == -1: raise PcapError(libpcap.pcap_geterr(pcap_t))
 	elif err == -2: raise StopIteration()
 	return err
-def NullCheck(pcap_t, res, func, args):
+
+def _chk_null(pcap_t, res, func, args):
 	if not res: raise PcapError(libpcap.pcap_geterr(pcap_t))
 	return res
 
@@ -37,7 +39,7 @@ def dumps(pkt_hdr, pkt):
 
 def loads(dump):
 	dump = xdrlib.Unpacker(dump)
-	pkt_hdr = pcap_pkthdr()
+	pkt_hdr = c_pcap_pkthdr()
 	pkt_hdr.ts.tv_sec, pkt_hdr.ts.tv_usec = dump.unpack_farray(2, dump.unpack_int)
 	pkt_hdr.caplen, pkt_hdr.len = dump.unpack_farray(2, dump.unpack_uint)
 	pkt = dump.unpack_bytes()
@@ -50,7 +52,7 @@ libpcap.pcap_open_offline.restype = ctypes.c_void_p
 libpcap.pcap_open_dead.restype = ctypes.c_void_p
 libpcap.pcap_dump_open.restype = ctypes.c_void_p
 libpcap.pcap_next_ex.argtypes = ctypes.c_void_p,\
-	ctypes.POINTER(ctypes.POINTER(pcap_pkthdr)),\
+	ctypes.POINTER(ctypes.POINTER(c_pcap_pkthdr)),\
 	ctypes.POINTER(ctypes.POINTER(ctypes.c_char))
 
 
@@ -59,8 +61,9 @@ def reader(path='-', opaque=True):
 	src = libpcap.pcap_open_offline(path, errbuff)
 	if not src: raise PcapError(errbuff.value)
 	try:
-		libpcap.pcap_next_ex.errcheck = ft.partial(ReadCheck, src)
-		pkt_hdr_p, pkt_p = ctypes.POINTER(pcap_pkthdr)(), ctypes.POINTER(ctypes.c_char)()
+		libpcap.pcap_next_ex.errcheck = ft.partial(_chk_read, src)
+		pkt_hdr_p, pkt_p = ctypes.POINTER(
+			c_pcap_pkthdr )(), ctypes.POINTER(ctypes.c_char)()
 		while True:
 			libpcap.pcap_next_ex(src, ctypes.byref(pkt_hdr_p), pkt_p)
 			pkt_hdr = pkt_hdr_p.contents
@@ -72,7 +75,7 @@ def writer(path='-', opaque=True):
 	dst = libpcap.pcap_open_dead(0, 65535) # linktype=DLT_NULL, snaplen
 	if not dst: raise PcapError
 	try:
-		libpcap.pcap_dump_open.errcheck = ft.partial(NullCheck, dst)
+		libpcap.pcap_dump_open.errcheck = ft.partial(_chk_null, dst)
 		dumper = libpcap.pcap_dump_open(dst, path)
 		while True:
 			dump = yield
